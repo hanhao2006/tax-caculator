d668b7383430830c3a2b73d69ab4e1ce1f65a06e
hanhao2006 <hanhao25@outlook.com>
2023-09-07 16:39:51 -0400

    click number done

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 ### Tax - Calculator - Montreal
 
 ![Calculator!](./public/imgs/simple.png)
+
+### Task
+
+1. When the user clicks the number button, the display field shows the value of the button.
+2. When the user clicks the AC button, the display field is cleared.
+
+### Problems
+
+1. When the user clicks the < button, it is able to remove last number or operator
+   > - using clear state, only remove the previous number, no working in current number
--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
+import { useState } from 'react'
+
 export default function App() {
+  // For all the keys
+  const [operator, setOperator] = useState('')
+
+  // For button AC
+  const [ac, setAc] = useState(false)
+
+  // set Operator valuable from KeyBorders Child
+  function handleOperator(value) {
+    ac ? setOperator('') : setOperator((op) => [...op, value])
+    setAc(false)
+  }
+  // if the AC button click
+  function handleAC() {
+    setAc(!ac)
+  }
+
   return (
     <div className='containe'>
       <Header />
-      <DisplayExpression />
-      <Keyborder />
+      <DisplayExpression operator={operator} />
+      <Keyborder onOperator={handleOperator} onAC={handleAC} />
     </div>
   )
 }
@@ -12,10 +30,10 @@ function Header() {
   return <h1>Tax - Calculator - Montreal</h1>
 }
 
-function DisplayExpression() {
+function DisplayExpression({ operator }) {
   return (
     <div className='display'>
-      <p> hello</p>
+      <p>{operator}</p>
       <DisplayResult />
     </div>
   )
@@ -29,30 +47,62 @@ function DisplayResult() {
   )
 }
 
-function Keyborder() {
+function Keyborder({ onOperator, onAC }) {
   const operators = ['+', '-', '*', '/']
+  const [keyBorder, setKeyBorder] = useState('')
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    onOperator(keyBorder)
+  }
   return (
     <>
-      <div className='bottons'>
+      <form className='bottons' onSubmit={handleSubmit}>
         <div className='btn main-operators'>
-          <input type='button' value='AC' />
-          <input type='button' value='+/-' />
-          <input type='button' value='1.5' />
-          <input type='button' value='%' />
+          <input type='submit' value='AC' onClick={onAC} />
+          <input
+            type='submit'
+            value='+/-'
+            onClick={(e) => setKeyBorder(e.target.value)}
+          />
+          <input
+            type='submit'
+            value='1.5'
+            onClick={(e) => setKeyBorder(e.target.value)}
+          />
+          <input type='submit' value='<' disabled />
         </div>
         <div className='btn numbers-operators'>
           {Array.from({ length: 10 }, (_, i) => 9 - i).map((i) => (
-            <input type='button' value={i} key={i} />
+            <input
+              type='submit'
+              value={i}
+              key={i}
+              onClick={(e) => setKeyBorder(e.target.value)}
+            />
           ))}
-          <input type='button' value='.' />
-          <input type='button' value='=' />
+          <input
+            type='submit'
+            value='.'
+            onClick={(e) => setKeyBorder(e.target.value)}
+          />
+          <input
+            type='submit'
+            value='='
+            onClick={(e) => setKeyBorder(e.target.value)}
+          />
         </div>
         <div className='btn op-operators'>
           {operators.map((op, index) => (
-            <input type='button' value={op} key={index} />
+            <input
+              type='submit'
+              value={op}
+              key={index}
+              onClick={(e) => setKeyBorder(e.target.value)}
+            />
           ))}
         </div>
-      </div>
+      </form>
     </>
   )
 }
